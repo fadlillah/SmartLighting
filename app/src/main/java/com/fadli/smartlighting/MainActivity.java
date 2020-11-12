2f777327554a82fd66dd6a496a5ab065bbad2663
fadlillah <muhammadfadlullah9@gmail.com>
2020-11-12 23:42:45 +0700

    update again

--- app/src/main/java/com/fadli/smartlighting/MainActivity.java
+++ app/src/main/java/com/fadli/smartlighting/MainActivity.java
@@ -1,6 +1,8 @@
 package com.fadli.smartlighting;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListAdapter;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -10,13 +12,43 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-    @Override
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<id_lamp> data;
+    DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+
+//    public void getLamp(String uID) {
+//
+//    }
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        //show log data from firebase
+        mDatabase.child("lamp").addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        data = new ArrayList<>();
+                        for (DataSnapshot noteDataSnapshot : dataSnapshot.getChildren()){
+                            Log.i(noteDataSnapshot.getValue().toString(), "log data");
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        // Getting Post failed, log a message
+                    }
+                });
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
--- app/src/main/java/com/fadli/smartlighting/id_lamp.java
+++ app/src/main/java/com/fadli/smartlighting/id_lamp.java
@@ -1,8 +1,12 @@
 package com.fadli.smartlighting;
 
 public class id_lamp {
-    public String id;
-    public id_lamp(String id){
+    public int id;
+    public Double Latitude;
+    public Double Longitude;
+    public int intensitasCahaya;
+    public String status;
+    public id_lamp(int id){
         this.id = id;
     }
 }
